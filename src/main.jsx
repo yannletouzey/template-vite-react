a1a1983dc63557248f77b129d6fb9c3f8e4137f1
yannletouzey <y.letouzey@gmail.com>
2025-02-09 10:49:54 +0100

    move file css and img

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/index.jsx'
-import './assets/css/style.css'
-import './assets/css/reset.css'
+import './css/style.css'
+import './css/reset.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
